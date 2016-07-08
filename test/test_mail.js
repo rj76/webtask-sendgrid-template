0b504d08c6c05b1d0725a67e11ecc8e5dd98164b
Richard Jansen <richard@jansenit.com>
2016-07-08 16:25:36 +0200

    async test

--- test/test_mail.js
+++ test/test_mail.js
@@ -2,7 +2,7 @@ var mail = require('../index.js');
 var base64url = require('base64url');
 var assert = require('chai').assert;
 
-describe('Test sendgrid mail template', function() {
+describe('Test sendgrid mail template', function(done) {
 	it('should send an email and return HTTP 202', function() {
 		mail({
 			b: base64url.encode('Hello world'),
@@ -12,6 +12,7 @@ describe('Test sendgrid mail template', function() {
 			secrets: {SG_KEY: process.env.SENDGRID_API_KEY}
 		}, function(err, response) {
 			assert(response.statusCode, 202);
+			done();
 		});
 	});
 });
