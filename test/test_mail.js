397e8f0da08febe81e8fbc5d657aa3277603965b
Richard Jansen <richard@jansenit.com>
2016-07-08 14:46:27 +0200

    added simple test case for sending the email

---
+++ test/test_mail.js
@@ -0,0 +1,15 @@
+var mail = require('../index.js');
+var base64url = require('base64url');
+var assert = require('chai').assert;
+
+describe('Test sendgrid mail template', function() {
+	it('should send an email and return HTTP 202', function() {
+		mail({
+			b: base64url.encode('Hello world'),
+			s: base64url.encode('Test subject'),
+			t: 'richard@jansenit.com'
+		}, function(err, response) {
+			assert(response.statusCode, 202);
+		});
+	});
+});
